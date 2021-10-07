3d368335d318755469d942cf1c63484909324189
jguerreiro <joao.guerreiro@gitguardian.com>
2021-10-07 11:26:07 +0200

    add secret

--- python_server.py
+++ python_server.py
@@ -7,7 +7,7 @@ Usage::
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 
-test_slack_key = "reset"
+test_slack_key = "xoxb-183213206324-FGqsdMF8t18v6N7Oq4i99vs5"
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
